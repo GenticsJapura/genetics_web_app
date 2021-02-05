88c614236712e220d6cc4b86b2cc419365bca7f0
Hemal <h.githmin@gmail.com>
2021-02-05 19:29:36 +0530

    [Home] - added  - header

--- geneticswebapp/src/App.js
+++ geneticswebapp/src/App.js
@@ -1,10 +1,37 @@
 import './App.css'; 
+import React from 'react';
+import Particles from "react-particles-js"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <Navbar/>
+    <React.Fragment>
+      <Particles
+        params={{
+          particles:{
+            number:{
+              value:30,
+              density:{
+                enable:true,
+                value_area:900
+              }
+            }
+          },
+          shape:{
+            type:"circle",
+            stroke:{
+              width :6,
+              color:"#f9ab00"
+            }
+          }
+        }}
+      />
+       <Navbar/>
+       <Header />
+    </React.Fragment>
+   
   );
 }
 
