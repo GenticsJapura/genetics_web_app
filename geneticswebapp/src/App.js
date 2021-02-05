7a9ed1934e1fc247b37f1acfe1a14933e6e0f08d
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-05 19:02:31 +0530

    created header for about section

--- geneticswebapp/src/App.js
+++ geneticswebapp/src/App.js
@@ -1,10 +1,14 @@
-import './App.css'; 
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import AboutUs from "./components/AboutComponent/AboutUs";
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar />
+      <AboutUs />
+    </div>
   );
 }
 
