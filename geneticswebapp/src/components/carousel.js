0610530886dc52fd6a4f0da2b9cd62267b7d7974
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-07 14:48:08 +0530

    carousel error fixed

--- geneticswebapp/src/components/carousel.js
+++ geneticswebapp/src/components/carousel.js
@@ -1,38 +1,31 @@
 import React from "react";
-import {Carousel,Carousel.Item} from 'react-bootstrap/Carousel'
+
+import Carousel from "react-bootstrap/Carousel";
+
+import ImageOne from "./images/img1.jpg";
+import ImageTwo from "./images/img2.jpg";
+import ImageThree from "./images/img3.jpg";
 
 const CarouselDemo = () => {
   return (
     <div>
       <Carousel>
         <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src="./images/img1.jpg"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={ImageOne} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-            src="./images/img2.jpg"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={ImageTwo} alt="Third slide" />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="./images/img3.jpg"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={ImageThree} alt="Third slide" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
