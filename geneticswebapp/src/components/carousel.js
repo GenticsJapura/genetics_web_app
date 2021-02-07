cfcfcad99fd0ac1f24bf9865a7e20a258ad47e81
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-07 20:52:58 +0530

    changes

--- geneticswebapp/src/components/carousel.js
+++ geneticswebapp/src/components/carousel.js
@@ -11,15 +11,27 @@ const CarouselDemo = () => {
     <div className="corouseldemo container mt-5 mb-5">
       <Carousel>
         <Carousel.Item interval={1000}>
-          <img className="d-block w-100" src={ImageOne} alt="First slide" />
+          <img
+            className="d-block w-100 CaroselImage"
+            src={ImageOne}
+            alt="First slide"
+          />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img className="d-block w-100" src={ImageTwo} alt="Third slide" />
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100 CaroselImage"
+            src={ImageTwo}
+            alt="Third slide"
+          />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={ImageThree} alt="Third slide" />
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100 CaroselImage"
+            src={ImageThree}
+            alt="Third slide"
+          />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
       </Carousel>
