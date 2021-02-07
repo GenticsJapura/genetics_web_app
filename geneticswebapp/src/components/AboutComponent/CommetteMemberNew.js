772ab8130f9fb671c911675ba7daf1ba6fe0a450
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-07 08:16:58 +0530

    updated aboutus.css

--- geneticswebapp/src/components/AboutComponent/CommetteMemberNew.js
+++ geneticswebapp/src/components/AboutComponent/CommetteMemberNew.js
@@ -6,14 +6,19 @@ import member2 from "./img/member2.jpg";
 export default function CommetteMember() {
   return (
     <div>
-      <div className="row">
+      <div className="row AboutUsComponent">
         <div className="col-md-12 text-center">
           <h3>COMMITTEE MEMBERS</h3>
           <hr className="HrBreaker" />
         </div>
-
         {/* Column One Start */}
-        <div className="col-md-3 AboutUsComponent">
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
           <div className="text-center">
             <img
               src={member1}
@@ -25,13 +30,19 @@ export default function CommetteMember() {
           <div class="card-body text-center">
             <p class="card-text">Senura Jayadeva</p>
             <p class="card-text ">
-              <span className="memberrole">President</span>
+              <span className="memberrole">PRESIDENT</span>
             </p>
           </div>
         </div>
         {/* Column One End */}
         {/* Column One Start */}
-        <div className="col-md-3 AboutUsComponent">
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
           <div className="text-center">
             <img
               src={member1}
@@ -43,13 +54,19 @@ export default function CommetteMember() {
           <div class="card-body text-center">
             <p class="card-text">Senura Jayadeva</p>
             <p class="card-text ">
-              <span className="memberrole">President</span>
+              <span className="memberrole">PRESIDENT</span>
             </p>
           </div>
         </div>
         {/* Column One End */}
         {/* Column One Start */}
-        <div className="col-md-3 AboutUsComponent">
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
           <div className="text-center">
             <img
               src={member1}
@@ -61,13 +78,19 @@ export default function CommetteMember() {
           <div class="card-body text-center">
             <p class="card-text">Senura Jayadeva</p>
             <p class="card-text ">
-              <span className="memberrole">President</span>
+              <span className="memberrole">PRESIDENT</span>
             </p>
           </div>
         </div>
         {/* Column One End */}
         {/* Column One Start */}
-        <div className="col-md-3 AboutUsComponent">
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
           <div className="text-center">
             <img
               src={member1}
@@ -79,7 +102,127 @@ export default function CommetteMember() {
           <div class="card-body text-center">
             <p class="card-text">Senura Jayadeva</p>
             <p class="card-text ">
-              <span className="memberrole">President</span>
+              <span className="memberrole">PRESIDENT</span>
+            </p>
+          </div>
+        </div>
+        {/* Column One End */}
+        {/* Column One Start */}
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          <div className="text-center">
+            <img
+              src={member1}
+              class="card-img-top MemberImage"
+              alt="memeber1"
+            />
+          </div>
+
+          <div class="card-body text-center">
+            <p class="card-text">Senura Jayadeva</p>
+            <p class="card-text ">
+              <span className="memberrole">PRESIDENT</span>
+            </p>
+          </div>
+        </div>
+        {/* Column One End */}
+        {/* Column One Start */}
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          <div className="text-center">
+            <img
+              src={member1}
+              class="card-img-top MemberImage"
+              alt="memeber1"
+            />
+          </div>
+
+          <div class="card-body text-center">
+            <p class="card-text">Senura Jayadeva</p>
+            <p class="card-text ">
+              <span className="memberrole">PRESIDENT</span>
+            </p>
+          </div>
+        </div>
+        {/* Column One End */}
+        {/* Column One Start */}
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          <div className="text-center">
+            <img
+              src={member1}
+              class="card-img-top MemberImage"
+              alt="memeber1"
+            />
+          </div>
+
+          <div class="card-body text-center">
+            <p class="card-text">Senura Jayadeva</p>
+            <p class="card-text ">
+              <span className="memberrole">PRESIDENT</span>
+            </p>
+          </div>
+        </div>
+        {/* Column One End */}
+        {/* Column One Start */}
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          <div className="text-center">
+            <img
+              src={member1}
+              class="card-img-top MemberImage"
+              alt="memeber1"
+            />
+          </div>
+
+          <div class="card-body text-center">
+            <p class="card-text">Senura Jayadeva</p>
+            <p class="card-text ">
+              <span className="memberrole">PRESIDENT</span>
+            </p>
+          </div>
+        </div>
+        {/* Column One End */}
+        {/* Column One Start */}
+        <div
+          className="col-lg-3 col-md-4 AboutUsComponent"
+          data-aos="zoom-in"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          <div className="text-center">
+            <img
+              src={member1}
+              class="card-img-top MemberImage"
+              alt="memeber1"
+            />
+          </div>
+
+          <div class="card-body text-center">
+            <p class="card-text">Senura Jayadeva</p>
+            <p class="card-text ">
+              <span className="memberrole">PRESIDENT</span>
             </p>
           </div>
         </div>
