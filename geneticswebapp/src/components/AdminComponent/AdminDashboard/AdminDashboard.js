7beee3d22624fb8d0ade52fbece7934a2b7b78ba
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-11 19:50:05 +0530

    change scroll bar

--- geneticswebapp/src/components/AdminComponent/AdminDashboard/AdminDashboard.js
+++ geneticswebapp/src/components/AdminComponent/AdminDashboard/AdminDashboard.js
@@ -1,14 +1,5 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
-import MembersImage from "../img/member.png";
 
 import "../admin.css";
 
